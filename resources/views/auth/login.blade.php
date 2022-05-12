61be0bd6b0e96aedf13b356dba7972ffd4895499
Ngo Quoc Dat <datlechin@gmail.com>
2022-05-13 00:14:56 +0700

    Clean resources
    Signed-off-by: Ngo Quoc Dat <datlechin@gmail.com>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
         <div class="bg-overlay"></div>
 
         <div class="shape">
-            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1440 120">
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1440 120">
                 <path d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"></path>
             </svg>
         </div>
@@ -114,9 +114,3 @@
     </footer>
 </div>
 @endsection
-
-@push('scripts')
-<script src="{{ asset('assets/libs/particles.js/particles.js.min.js') }}"></script>
-<script src="{{ asset('assets/js/pages/particles.app.js') }}"></script>
-<script src="{{ asset('assets/js/pages/password-addon.init.js') }}"></script>
-@endpush
