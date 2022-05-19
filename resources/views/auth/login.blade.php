05ed32e94ae2f00a518845a710a881e5f42320a9
Ngo Quoc Dat <datlechin@gmail.com>
2022-05-19 21:08:08 +0700

    wip

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,11 +19,9 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="text-center mt-sm-5 mb-4 text-white-50">
-                        <div>
-                            <a href="{{ route('home') }}" class="d-inline-block auth-logo">
-                                <img src="{{ URL::asset('assets/images/logo-light.png')}}" alt="" height="20">
-                            </a>
-                        </div>
+                        <a href="{{ route('home') }}" class="d-inline-block auth-logo">
+                            <img src="{{ URL::asset('assets/images/logo-light.png')}}" alt="" height="20">
+                        </a>
                         <p class="mt-3 fs-15 fw-medium">Premium Admin & Dashboard Template</p>
                     </div>
                 </div>
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,90 @@
-@extends('layouts.app')
+@extends('layouts.auth-master')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+    <div class="auth-page-wrapper pt-5">
+        <div class="auth-one-bg-position auth-one-bg">
+            <div class="bg-overlay"></div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+            <div class="shape">
+                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1440 120">
+                    <path d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"></path>
+                </svg>
+            </div>
+        </div>
+
+        <div class="auth-page-content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center mt-sm-5 mb-4 text-white-50">
+                            <a href="{{ route('home') }}" class="d-inline-block auth-logo">
+                                <img src="{{ asset('assets/images/logo-light.png') }}" alt="" height="20" />
+                            </a>
+                            <p class="mt-3 fs-15 fw-medium">Premium Admin & Dashboard Template</p>
                         </div>
-                    @endif
+                    </div>
+                </div>
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                <div class="row justify-content-center">
+                    <div class="col-md-8 col-lg-6 col-xl-5">
+                        <div class="card mt-4">
+                            <div class="card-body p-4">
+                                <div class="text-center mt-2">
+                                    <h5 class="text-primary">Lấy lại mật khẩu</h5>
+                                    <lord-icon src="https://cdn.lordicon.com/rhvddzym.json" trigger="loop" colors="primary:#0ab39c" class="avatar-xl"> </lord-icon>
+                                </div>
+                                <div class="alert alert-borderless alert-warning text-center mb-2 mx-2" role="alert">
+                                    Nhập địa chỉ email của bạn để lấy lại mật khẩu.
+                                </div>
+                                <div class="p-2">
+                                    <form action="{{ route('password.email') }}" method="post">
+                                        @csrf
+                                        <div class="mb-4">
+                                            <label class="form-label">Email</label>
+                                            <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder="Nhập địa chỉ email" value="{{ old('email') }}" />
+                                            @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+                                        <div class="text-center mt-4">
+                                            <button class="btn btn-success w-100" type="submit">Gửi liên kết</button>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
+                        <div class="mt-4 text-center">
+                            <p class="mb-0">
+                                Bạn nhớ mật khẩu?
+                                <a href="{{ route('login') }}" class="fw-semibold text-primary text-decoration-underline">
+                                    Đăng nhập
+                                </a>
+                            </p>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
+
+        <footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center">
+                            <p class="mb-0 text-muted">
+                                &copy;
+                                <script>
+                                    document.write(new Date().getFullYear());
+                                </script>
+                                Velzon. Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </div>
-</div>
 @endsection
