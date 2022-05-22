c5432c025893003ffd4513f0bb5df5fe105adeda
Ngo Quoc Dat <datlechin@gmail.com>
2022-05-22 11:14:42 +0700

    uppdate

--- app/helpers.php
+++ app/helpers.php
@@ -5,16 +5,11 @@ if (! function_exists('get_browser_name')) {
     {
         $browser = 'Unknown';
         $browser_array = array(
-            '/msie/i' => 'Internet Explorer',
             '/firefox/i' => 'Firefox',
             '/safari/i' => 'Safari',
             '/chrome/i' => 'Chrome',
             '/edge/i' => 'Edge',
             '/opera/i' => 'Opera',
-            '/netscape/i' => 'Netscape',
-            '/maxthon/i' => 'Maxthon',
-            '/konqueror/i' => 'Konqueror',
-            '/mobile/i' => 'Handheld Browser'
         );
         foreach ($browser_array as $regex => $value) {
             if (preg_match($regex, $user_agent)) {
@@ -30,12 +25,15 @@ if (! function_exists('get_device_name')) {
     {
         $device = 'Unknown';
         $device_array = array(
+            '/windows/i' => 'Windows',
             '/windows nt 10/i' => 'Windows 10',
             '/linux/i' => 'Linux',
             '/ubuntu/i' => 'Ubuntu',
             '/iphone/i' => 'iPhone',
             '/ipad/i' => 'iPad',
             '/android/i' => 'Android',
+            '/android 10/i' => 'Android 10',
+            '/android 12/i' => 'Android 12',
         );
         foreach ($device_array as $regex => $value) {
             if (preg_match($regex, $user_agent)) {
