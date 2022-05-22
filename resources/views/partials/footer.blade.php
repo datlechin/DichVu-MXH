e6c03915d680739fac0f62cbba007fd0732abf04
Ngo Quoc Dat <datlechin@gmail.com>
2022-05-22 13:57:19 +0700

    wip

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,11 +2,11 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-6">
-                <script>document.write(new Date().getFullYear())</script> © Velzon.
+                {{ now()->format('Y') }} © <a href="https://github.com/datlechin/DichVu-MXH" target="_blank">DVMXH</a>.
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
-                    Design & Develop by Themesbrand
+                    Phát triển bởi <a href="https://ngoquocdat.com" target="_blank">Ngô Quốc Đạt</a>
                 </div>
             </div>
         </div>
