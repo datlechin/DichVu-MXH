49fe0715f2e671483c8083bd60df531fb8c17e26
Ngo Quoc Dat <datlechin@gmail.com>
2022-05-23 22:15:55 +0700

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,8 @@
 
 namespace App\Providers;
 
-use App\Models\Category;
 use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         Paginator::useBootstrapFive();
     }
 }
