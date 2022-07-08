2de63ebf6480750e7fa96cd4d9aa624c654053ed
Ngo Quoc Dat <datlechin@gmail.com>
2022-07-08 21:57:47 +0700

    Fix counter not working

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,5 @@
 let navbarMenuHTML = document.querySelector('.navbar-menu').innerHTML;
 document.getElementById("topnav-hamburger-icon").addEventListener('click', toggleHamburgerMenu);
-isLoadBodyElement()
-initLeftMenuCollapse()
 const backToTopButton = document.getElementById("back-to-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -71,6 +69,43 @@ function initLeftMenuCollapse() {
     }
 }
 
+function counter() {
+    let counter = document.querySelectorAll('.counter-value');
+    let speed = 250; // The lower the slower
+    counter && counter.forEach(function (counter_value) {
+        function updateCount() {
+            let target = +counter_value.getAttribute('data-target');
+            let count = +counter_value.innerText;
+            let inc = target / speed;
+            if (inc < 1) {
+                inc = 1;
+            }
+            // Check if target is reached
+            if (count < target) {
+                // Add inc to count and output in counter_value
+                counter_value.innerText = (count + inc).toFixed(0);
+                // Call function every ms
+                setTimeout(updateCount, 1);
+            } else {
+                counter_value.innerText = numberWithCommas(target);
+            }
+            numberWithCommas(counter_value.innerText);
+        };
+        updateCount();
+    });
+
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+}
+
+function init() {
+    isLoadBodyElement()
+    initLeftMenuCollapse()
+    counter()
+}
+
+init();
 
 let ckClassicEditor = document.querySelectorAll(".ckeditor-classic")
 ckClassicEditor.forEach(function () {
